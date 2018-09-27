55148ca346ea800c471643f7ccaf4aca6929ec28
soulomoon <fwy996602672@gmail.com>
2018-09-27 12:20:54 +0800

    update alias

--- alias.zsh
+++ alias.zsh
@@ -27,3 +27,4 @@ alias cnpm="npm --registry=https://registry.npm.taobao.org \
 --disturl=https://npm.taobao.org/dist \
 --userconfig=$HOME/.cnpmrc"
 alias runghc="stack runghc"
+alias ghcrepl="stack exec -- ghci-color"
